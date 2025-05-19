058d448f71ce1ab8b3dcff176e9c9d02fde8f4d9
Erick Escamilla Charco <erescamillac@gmail.com>
2025-05-19 16:52:32 -0600

    Se agregan las operaciones de multiplicar y dividir en el archivo [calculadora.js]

--- calculadora.js
+++ calculadora.js
@@ -6,5 +6,14 @@ function restar(a, b){
     return a - b;
 }
 
+function multiplicar(a, b){
+    return a * b;
+}
+
+function dividir(a, b){
+    if (b === 0) throw new Error("¡División entre CERO!");
+    return a / b;
+}
+
 sumar(5, 3);
 restar(10, 6);
